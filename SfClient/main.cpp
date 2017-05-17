4fd9faa18e5cd45cc851ecc75dc804e40692259e
spplus <yleesun@163.com>
2017-05-17 15:11:17 +0800

    升级版本号
    升级版本号

--- SfClient/main.cpp
+++ SfClient/main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	QApplication::addLibraryPath("./plugins");
 
 	// 设置程序版本号
-	QCoreApplication::setApplicationVersion("1.0.6");
+	QCoreApplication::setApplicationVersion("1.0.7");
 
 #ifdef WIN32
 	// 设置编码
